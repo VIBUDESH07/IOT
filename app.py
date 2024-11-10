b629b474896809770204033ec376863ca1e8c16b
VIBUDESH07 <125853344+VIBUDESH07@users.noreply.github.com>
2024-11-10 23:53:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -127,4 +127,5 @@ def twiml():
     return response, 200, {'Content-Type': 'application/xml'}
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+   
+    app.run(debug=True, host='0.0.0.0',  port = int(os.getenv("PORT", 5000)) )
